c32b2b6098cf086315afe44d6097f4bbedf8b0ed
dnvishnu12 <vishnu.dn@dataknobs.com>
2023-10-01 19:07:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="glitch-wrapper">
+   <span class="glitch" data-glitch="Dataknobs">Dataknobs</span>
+   <span class="glitch" id="dot">.</span>
+  
+</div>
+
+    </div>
+  );
+}
+
+export default App;
